41deab9f42c108b525dae22d54a4ccf2a05aaf11
Lucas Samuel F. S <68969699+Dev3lopmentM4chine@users.noreply.github.com>
2023-06-06 11:58:38 -0300

    update

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,12 @@
-import express, { Application } from "express";
 import "express-async-errors";
+import express from "express";
 import { HandleErrors } from "./errors";
 import usersRoute from "./routes/users.routes";
 import loginRouter from "./routes/login.routes";
 import contactsRouter from "./routes/contacts.routes";
 const cors = require("cors")
 
-const app: Application = express();
+const app = express();
 app.use(express.json());
 app.use(cors())
 
