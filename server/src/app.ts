b0f179262721376b1b7f892d488c8d623177041f
Lucas Samuel F. S <68969699+Dev3lopmentM4chine@users.noreply.github.com>
2023-06-06 12:18:47 -0300

    update

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,9 +4,10 @@ import { HandleErrors } from "./errors";
 import usersRoute from "./routes/users.routes";
 import loginRouter from "./routes/login.routes";
 import contactsRouter from "./routes/contacts.routes";
-const cors = require("cors")
 
+const cors = require("cors")
 const app = express();
+
 app.use(express.json());
 app.use(cors())
 
